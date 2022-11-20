e03c4c1e50965bc825aa0ef8ed95e3675beeb560
Irina <lebeirin@yandex.ru>
2022-11-20 23:20:02 +0500

    first commit

---
+++ 41/Program.cs
@@ -0,0 +1,16 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.Clear();
+Console.WriteLine("Введите количество чисел, которое неоходимо проверить");
+int ArrayLengh = int.Parse(Console.ReadLine());
+double [] nums = new double [ArrayLengh];
+Console.WriteLine("Введите числа через enter и программа покажет сколько было введено положительных чисел");
+for (int i=0; i<ArrayLengh;i++)
+{
+    nums [i] = double.Parse(Console.ReadLine());
+}
+int plus =0;
+for (int i=0; i<ArrayLengh;i++)
+{
+    if (nums [i]>0) plus++;
+}
+Console.WriteLine($"Было введено {plus} положительных чисел");
---
+++ 43/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+Console.Clear();
+Console.WriteLine("Программа ищет точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; введите значения k1, b1, k2 и b2");
+double [,] Coefficient = new double [2,2];
+void Explain (int k, int b)
+{
+    if (b==0) Console.Write($"k{k+1} = ");
+    else Console.Write($"b{k+1} = ");
+}
+for (int k =0; k<Coefficient.GetLength(0); k++)
+{
+    for (int b =0; b<Coefficient.GetLength(1); b++)
+    {
+        Explain (k, b);
+        Coefficient [k,b] = double.Parse(Console.ReadLine());
+    }
+}
+double [] Coordinates = new double [2];
+if (Coefficient[0, 0] == Coefficient [1,0]) Console.WriteLine("Линии параллельны");
+else
+{
+    Coordinates [0] = (Coefficient[1,1]-Coefficient[0,1])/(Coefficient[0,0]-Coefficient[1,0]);
+    Coordinates [1] = Coefficient[0,0]*Coordinates[0] +Coefficient[0,1];
+    Console.WriteLine($"Прямые пересекаются в точке [{Coordinates [0]}; {Coordinates [1]}]");
+}
